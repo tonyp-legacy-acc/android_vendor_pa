0298d755b4fb68f56b5620884fb2ad2ff169afbf
tonypp <text2tony@gmail.com>
2012-10-08 14:57:48 +0200

    remove pa backuptool overlay

--- config/pa_common.mk
+++ config/pa_common.mk
@@ -6,7 +6,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Replace CM files
 PRODUCT_COPY_FILES += \
-    vendor/pa/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/pa/prebuilt/common/apk/GooManager.apk:system/app/GooManager.apk \
 	vendor/pa/prebuilt/common/apk/ParanoidPreferences.apk:system/app/ParanoidPreferences.apk \
 	vendor/pa/prebuilt/common/apk/PDroid.apk:system/app/PDroid.apk \

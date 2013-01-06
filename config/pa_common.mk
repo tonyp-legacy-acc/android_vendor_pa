c8aae7815123e6fdcd8a1dfb9c24ad9c7cb24ac4
tonyp <text2tony@gmail.com>
2013-01-07 00:17:25 +0100

    fix tab from PDroid commit
    fixes ca51adc26ab07cd3e8fbda9787601f14c3effee5

--- config/pa_common.mk
+++ config/pa_common.mk
@@ -9,7 +9,7 @@ PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/pa/prebuilt/common/apk/GooManager.apk:system/app/GooManager.apk \
     vendor/pa/prebuilt/common/apk/SuperSU.apk:system/app/SuperSU.apk \
-    vendor/pa/prebuilt/common/apk/PDroid.apk:system/app/PDroid.apk \	
+    vendor/pa/prebuilt/common/apk/PDroid.apk:system/app/PDroid.apk \
     vendor/pa/prebuilt/common/xbin/su:system/xbin/su
 
 # Exclude prebuilt paprefs from builds if the flag is set
